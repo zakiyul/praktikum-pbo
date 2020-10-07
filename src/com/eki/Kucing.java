abb240c8e0125932ea29f15971f2bb62374674ec
zakiyul <zakiyul.py@gmail.com>
2020-10-07 09:59:18 +0800

    constructor

--- src/com/eki/Kucing.java
+++ src/com/eki/Kucing.java
@@ -1,16 +1,23 @@
 package com.eki;
 
 public class Kucing {
+
     public String warna = "orange";
     public int jumlah_kaki = 4;
     public String jenis_ekor = "panjang";
     public String makanan_kesukaan = "ikan";
 
-    public String bersuara(){
+    public Kucing(String warna,int jumlah_kaki,String jenis_ekor_kucing){
+        this.warna = warna;
+        this.jumlah_kaki = jumlah_kaki;
+        jenis_ekor = jenis_ekor_kucing;
+    }
+
+    public String bersuara(){  // method function
         return "meong...";
     }
 
-    public void berburu(){
-        System.out.println("berburu ikan");
+    public void berburu(String makanan){ // method prosedur
+        System.out.println("berburu "+makanan);
     }
 }
--- src/com/eki/Main.java
+++ src/com/eki/Main.java
@@ -3,9 +3,14 @@ package com.eki;
 public class Main {
 
     public static void main(String[] args) {
-        Kucing kucing1 = new Kucing();
+        Kucing kucing1 = new Kucing("purple",3,"belang");
         System.out.println("warna kucing1 adalah "+ kucing1.warna);
+        System.out.println("jumlah kaki "+kucing1.jumlah_kaki);
+        System.out.println("jenis ekor "+kucing1.jenis_ekor);
         System.out.println(kucing1.bersuara());
-        kucing1.berburu();
+        kucing1.berburu("pepaya");
+
+        kucing1.warna = "hijau";
+        System.out.println("kucing1 sekarang berubah warna menjadi "+kucing1.warna);
     }
 }
